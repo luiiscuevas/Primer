6dc1780f79e161cbd317794a81c8351f30d1ab0b
luiiscuevas <20131165@ce.pucmm.edu.do>
2017-05-20 16:47:14 -0400

    prueba

---
+++ src/main/java/Prueba/test/Main.java
@@ -0,0 +1,12 @@
+package Prueba.test;
+
+/**
+ * Created by luis_ on 20/5/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Por Fin");
+
+    }
+}
